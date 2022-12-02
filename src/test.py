058b781881cac0dcc5419d27e120a8bc9c7117c6
klei22 <klei22>
2022-12-01 21:48:00 -0800

    Create test for the rolling average
    This is a test for 8 element 5 bit rolling average, with an input (each
    cycle) of "5" ( or 0b101 in binary).

--- src/test.py
+++ src/test.py
@@ -2,6 +2,10 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
 
+moving_average_vals = []
+for i in range(10):
+    moving_average_vals.append((i*5) >> 3)
+
 @cocotb.test()
 async def test_7seg(dut):
     dut._log.info("start")
@@ -12,12 +16,15 @@ async def test_7seg(dut):
     dut.rst.value = 1
     await ClockCycles(dut.clk, 10)
     dut.rst.value = 0
+    dut.i_data_clk.value = 0
     await ClockCycles(dut.clk, 10)
 
     dut._log.info("check all segments")
-    assert int(dut.ra_out.value) == 0
-    # for i in range(5):
-    #     for j in range (5)
-    #     dut._log.info("check segment {}".format(i))
-    #     await ClockCycles(dut.clk, 100)
-        # assert int(dut.segments.value) == segments[i]
+    dut.i_value.value = 5
+    for i in range(10):
+        dut._log.info("ra value is: {}".format(dut.ra_out.value))
+        assert int(dut.ra_out.value) == moving_average_vals[i]
+        dut.i_data_clk.value = 0
+        await ClockCycles(dut.clk, 5)
+        dut.i_data_clk.value = 1
+        await ClockCycles(dut.clk, 5)
